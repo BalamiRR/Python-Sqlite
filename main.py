30c5cbf35d27bbf7a0c8f48bc0298f2ea19319a6
Fuat <fuatkara.95@gmail.com>
2018-03-17 06:01:07 +0100

    Adding students in Python with Sqlite..

---
+++ main.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+con = sqlite3.connect("Database.db")
+
+cursor = con.cursor()
+
+def createTable():
+    cursor.execute("CREATE TABLE Students (name TEXT,surName TEXT,number INT,grades INT)")
+    con.cursor()
+    con.close()
+
+def addValue():
+    cursor.execute("INSERT INTO Students VALUES('Mustafa','Ataturk',55,5.0)")
+    con.commit()
+    con.close()
+
+
+
+addValue()
