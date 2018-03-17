affecb88a5355eea86a72d2489c17e7b3921cf04
BalamiRR <36539306+BalamiRR@users.noreply.github.com>
2018-03-17 07:48:53 +0100

    Add files via upload
    Adding value in table

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
-import sqlite3
+import sqlite3 as sql
+import random as rd
+import time
+import datetime
 
-con = sqlite3.connect("Database.db")
 
+con = sql.connect("Time.db")
 cursor = con.cursor()
 
 def createTable():
-    cursor.execute("CREATE TABLE Students (name TEXT,surName TEXT,number INT,grades INT)")
-    con.cursor()
-    con.close()
+    cursor.execute("CREATE TABLE IF NOT EXISTS Tablo1 (times REAL,date TEXT,key TEXT,value REAL)")
 
-def addValue():
-    cursor.execute("INSERT INTO Students VALUES('Mustafa','Ataturk',55,5.0)")
+def addrandomly():
+    times = time.time()
+    date = str(datetime.datetime.fromtimestamp(times).strftime("%Y-%M-%D   %H-%M-%S"))
+    key = "Python Sqlite"
+    value = rd.randrange(0,10)
+    cursor.execute("INSERT INTO Tablo1 (times,date,key,value) VALUES(?,?,?,?)",(times,date,key,value))
     con.commit()
-    con.close()
-
 
 createTable()
-addValue()
+
+i=0
+while(i<10):
+    addrandomly()
+    time.sleep(1)
+    i+=1
+
+con.close()
